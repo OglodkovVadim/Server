1148b2d93ed59420cca725c696cd55aa9d9b5b0a
OglodkovVadim <oglodkovvadim@yandex.ru>
2023-09-24 19:47:56 +0300

    Added func to collecting post request from auth/ and inserting to db

--- main.cpp
+++ main.cpp
@@ -49,50 +49,61 @@ int main(int argc, char *argv[])
         return "Hello world";
     });
 
-    httpServer.route("/auth/", [](const QHttpServerRequest &request) {
+    httpServer.route("/resp/", [](const QHttpServerRequest &request) {
         QJsonDocument json;
-        QJsonArray recordsArray;
-        QSqlQuery query("SELECT * FROM UsersServer");
+        if (request.method() == QHttpServerRequest::Method::Get) {
+            QJsonArray recordsArray;
+            QSqlQuery query("SELECT * FROM UsersServer");
 
-        if (query.size() > 0)
-            while(query.next()) {
-                QJsonObject obj;
-                obj.insert("id", query.value(0).toString());
-                obj.insert("login", query.value(1).toString());
-                obj.insert("password", query.value(2).toString());
-                recordsArray.push_back(obj);
-            }
-        else
-            qDebug() << "There are not users";
+            if (query.size() > 0)
+                while(query.next()) {
+                    QJsonObject obj;
+                    obj.insert("id", query.value(0).toString());
+                    obj.insert("login", query.value(1).toString());
+                    obj.insert("password", query.value(2).toString());
+                    recordsArray.push_back(obj);
+                }
+            else
+                qDebug() << "There are not users";
 
-        json.setArray(recordsArray);
+            json.setArray(recordsArray);
+        }
         return json.toJson();
     });
 
-//    httpServer.route("/auth/", []() {
-//        QSqlQuery query("INSERT INTO UsersServer (Id, Login, Password) "
-//                        "VALUES ('alexey', 'shabarshov', 'alex228')");
-//        qDebug() << "Inserting complite";
-//        return "Auth Success";
-//    });
+    httpServer.route("/auth/", [](const QHttpServerRequest& request) {
+        QString messageText = "Faild";
+        auto status = QHttpServerResponse::StatusCode::BadRequest;
+        if (request.method() == QHttpServerRequest::Method::Post) {
+            QSqlQuery query;
+            query.prepare("INSERT INTO UsersServer (Id, Login, Password) "
+                          "VALUES (:id, :login, :password)");
+            query.bindValue(":id", QJsonDocument::fromJson(request.body()).object().value("id"));
+            query.bindValue(":login", QJsonDocument::fromJson(request.body()).object().value("login").toString());
+            query.bindValue(":password", QJsonDocument::fromJson(request.body()).object().value("password").toString());
+            if (query.exec()) {
+                messageText = "Success";
+                status = QHttpServerResponse::StatusCode::Ok;
+                qDebug() << "Inserting complite";
+            }
+            else
+                messageText = query.lastError().text();
+        }
+        return QHttpServerResponse {
+            QJsonObject {
+                {"message", messageText}
+            },
+            status
+        };
+    });
 
-//    httpServer.route("/resp", [] () {
-//        QSqlQuery query("SELECT * from UsersServer");
-//        if (query.size() > 0)
-//            while(query.next()) {
-//                qDebug() << query.value(0).toString();
-//                qDebug() << query.value(1).toString();
-//                qDebug() << query.value(2).toString();
-//            }
-//        else
-//            qDebug() << "There are not users";
+// return {
+//      { " message ": "error/success"}
+//    }
 
-//        return "Get data";
-//    });
 
     httpServer.route("/del", [] () {
         QSqlQuery query("DELETE from UsersServer");
-
         return "Deleted data :(";
     });
 
